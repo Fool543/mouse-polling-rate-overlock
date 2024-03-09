eb815d7256b0e67c6ed97cbe328a9a3966dac545
Fool543 <120022950+Fool543@users.noreply.github.com>
2024-03-09 21:51:47 +0530

    Add files via upload

---
+++ Start.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\atsiv\atsiv.exe" -f "C:\Program Files\atsiv\hidusbfp.sys"
+"C:\Program Files\atsiv\atsiv.exe" -u "C:\Program Files\atsiv\hidusbfp.sys"
+"C:\Program Files\atsiv\devcon.exe" restart *VID_046D*
