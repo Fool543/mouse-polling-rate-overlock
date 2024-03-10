ad5fa9670382441acada37900f68db9aa8b9a58a
Fool543 <120022950+Fool543@users.noreply.github.com>
2024-03-10 21:56:48 +0530

    Update Start.bat

--- Start.bat
+++ Start.bat
@@ -1,3 +1,4 @@
+@echo off
 "C:\Program Files\atsiv\atsiv.exe" -f "C:\Program Files\atsiv\hidusbfp.sys"
 "C:\Program Files\atsiv\atsiv.exe" -u "C:\Program Files\atsiv\hidusbfp.sys"
 "C:\Program Files\atsiv\devcon.exe" restart *VID_046D*
