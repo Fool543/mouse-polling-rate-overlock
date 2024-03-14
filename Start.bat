ac85b3386e642e950dc4351c125ea1ab1c699689
Fool543 <120022950+Fool543@users.noreply.github.com>
2024-03-14 22:02:43 +0530

    Update Start.bat

--- Start.bat
+++ Start.bat
@@ -1,8 +1,9 @@
 @echo off
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+timeout 10 /nobreak
 "C:\Program Files\atsiv\atsiv.exe" -f "C:\Program Files\atsiv\hidusbfp.sys"
-timeout 30 /nobreak
+timeout 10 /nobreak
 "C:\Program Files\atsiv\devcon.exe" restart *VID_046D*
 timeout 2 /nobreak
 exit
